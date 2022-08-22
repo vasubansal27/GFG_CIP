55db7737f1109299ea4a79ed78d01532f25ace66
vasubansal27 <111572815+vasubansal27@users.noreply.github.com>
2022-08-22 13:09:36 +0530

    Update StockSpan.cpp

--- STL/stack/StockSpan.cpp
+++ STL/stack/StockSpan.cpp
@@ -1,14 +1,20 @@
 for every element you have to count how many elements are there before it which are smaller than it 
 to count we store indices instead of value
 
+   vector <int> calculateSpan(int arr[], int n)
+    {
+       // Your code here
+       vector<int>v;
     stack<int>s;
     s.push(0);
+    v.push_back(1);
     for(int i=1;i<n;i++)
     {
       while(s.empty()==false && arr[i]>=arr[s.top()] )
           s.pop();
       int span=(s.empty())?(i+1):(i-s.top());
-      cout << span << " ";
+      v.push_back(span);
       s.push(i);
-        
+    }
+    return v;
     }
